62a7b985e8be28b0720d14d1c45ffa43789e39de
Miguel Mediza <migueamediza@gmail.com>
2025-09-14 21:15:42 -0300

    Configuracion necesaria para desplegar la web

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,9 @@
 import {createPool} from 'mysql2/promise';
-
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from './config.js';
 export const pool = new createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: 3306,
-    database: 'tesisdb'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_NAME
 });
